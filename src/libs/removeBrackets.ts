76b9c28585e7974cd45e6081c429b67d7a17c921
fokhei <fokhei@gmail.com>
2020-02-02 13:51:46 +0800

    improve google search

---
+++ src/libs/removeBrackets.ts
@@ -0,0 +1,7 @@
+export const removeBrackets = (str: string) => {
+  let out = str.replace(/\(.+\)/gi, "");
+  out = out.replace(/\[.+\]/gi, "");
+  out = out.replace(/\s{2,}/gi, " ");
+  out = out.trim();
+  return out;
+};
--- src/libs/toGoogleKeyword.ts
+++ src/libs/toGoogleKeyword.ts
@@ -1,5 +1,8 @@
+import { removeBrackets } from './removeBrackets';
+
 export const toGoogleKeyword = (str: string) => {
-  let keyword = str.replace(/[\s]/gi, "+");
+  let keyword = removeBrackets(str);
+  keyword = keyword.replace(/[\s]/gi, "+");
   keyword = encodeURIComponent(keyword);
   return keyword;
 };
